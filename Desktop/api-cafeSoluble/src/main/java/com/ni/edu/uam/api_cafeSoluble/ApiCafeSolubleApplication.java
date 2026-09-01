b130b89be28f409a374f3bd503618edf184f5820
Laboratorio C108 <labc108@uam.net.ni>
2026-08-31 19:41:32 -0600

    primer commit

---
+++ Desktop/api-cafeSoluble/src/main/java/com/ni/edu/uam/api_cafeSoluble/ApiCafeSolubleApplication.java
@@ -0,0 +1,13 @@
+package com.ni.edu.uam.api_cafeSoluble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCafeSolubleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCafeSolubleApplication.class, args);
+	}
+
+}
