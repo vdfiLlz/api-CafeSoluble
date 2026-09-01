c5ee2588db714c0e82cee6fa728ed0f9736737f7
Laboratorio C108 <labc108@uam.net.ni>
2026-08-31 20:52:34 -0600

    Clase controller creacion

---
+++ Desktop/api-cafeSoluble/src/main/java/com/ni/edu/uam/api_cafeSoluble/controller/ProductoController.java
@@ -0,0 +1,39 @@
+package com.ni.edu.uam.api_cafeSoluble.controller;
+
+import com.ni.edu.uam.api_cafeSoluble.models.Producto;
+import com.ni.edu.uam.api_cafeSoluble.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductoController {
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/productos")
+    public ResponseEntity<List<Producto>> getAll() {
+        return ResponseEntity.ok(productoService.findAll());
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> findById(@PathVariable("id") Long id) {
+        Producto producto = productoService.findById(id);
+        if (producto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(producto);
+    }
+
+    @PostMapping("/productos")
+    public ResponseEntity<Producto> save(@RequestBody Producto producto) {
+        Producto nuevo = productoService.save(producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+    }
+}
