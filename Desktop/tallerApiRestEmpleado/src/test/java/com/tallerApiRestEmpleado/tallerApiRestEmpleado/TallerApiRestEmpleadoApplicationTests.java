14f908a81393c6606de013e89ef323bb6781e723
Laboratorio C108 <labc108@uam.net.ni>
2026-08-31 19:25:11 -0600

    primer commit

---
+++ Desktop/tallerApiRestEmpleado/src/test/java/com/tallerApiRestEmpleado/tallerApiRestEmpleado/TallerApiRestEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerApiRestEmpleado.tallerApiRestEmpleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApiRestEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
